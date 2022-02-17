d2a666f38bdd572db3f35f2cc52532f4d352bd46
Arai-git <10l.mah.s11@gmail.com>
2022-02-17 10:48:55 +0900

    ファイル配下とブランチ名修正

---
+++ src/public/history.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="UTF-8">
+      <title>送信履歴</title>
+    </head>
+  <body>
+  <h1>送信履歴</h1>
+
+  <?php
+  //直リンクされた場合index.phpにリダイレクト
+  // if($_SERVER["REQUEST_METHOD"] != "POST"){
+  //   header("Location: index.php");
+  //   exit();
+  // }
+  $db_username = 'root';
+  $db_password = 'password';
+  $pdo = new pdo(
+      'mysql:host=mysql; dbname=contactform; charset=utf8',
+      $db_username,
+      $db_password
+  );
+  $sql = 'select * from contacts';
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute();
+  $results = $stmt->fetchall();
+
+  foreach ($results as $result) {
+      echo $result['title'];
+      echo '<br>';
+      echo '<br>';
+      echo $result['content'];
+      echo '<br>';
+      echo '<br>';
+  }
+  ?>
+
+  <p><a href="index.php">戻る</p>
+  </form>
+  </body>
+</html> 

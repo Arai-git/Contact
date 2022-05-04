0c222f9619ee4927e936e6dd59064e9036fd7680
Arai-git <10l.mah.s11@gmail.com>
2022-05-04 14:23:39 +0900

    DDD(Presenter, QueryService)実装に伴うコードの修正

--- src/public/history.php
+++ src/public/history.php
@@ -1,41 +1,45 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+use App\UseCase\UseCaseInput\ContactHistoryInput;
+use App\UseCase\UseCaseInteractor\ContactHistoryInteractor;
+use App\Adapter\Presenter\ContactHistoryPresenter;
+use App\Domain\ValueObject\SearchWord;
+
+$inputSearchWord = filter_input(INPUT_GET, 'searchWord');
+$searchWord = new SearchWord($inputSearchWord);
+$contactHistoryInput = new ContactHistoryInput($searchWord);
+$contactHistoryInteractor = new ContactHistoryInteractor($contactHistoryInput);
+$historyPresenter = new ContactHistoryPresenter(
+    $contactHistoryInteractor->handler()
+);
+$contacts = $historyPresenter->createHistoryView();
+?>
+
 <!DOCTYPE html>
-  <html>
-    <head>
-      <meta charset="UTF-8">
-      <title>送信履歴</title>
-    </head>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>送信履歴</title>
+</head>
+
   <body>
-  <h1>送信履歴</h1>
+    <div class="container">
+      <form method="get" action="./history.php">
+        <div>
+        <input name="searchWord" type="text" value="<?php echo $inputSearchWord ??
+            ''; ?>" placeholder="キーワードを入力" />
+        <input type="submit" value="検索" />
+        </div>
+      </form>
 
-  <?php
-  //直リンクされた場合index.phpにリダイレクト
-  // if($_SERVER["REQUEST_METHOD"] != "POST"){
-  //   header("Location: index.php");
-  //   exit();
-  // }
-  $db_username = 'root';
-  $db_password = 'password';
-  $pdo = new pdo(
-      'mysql:host=mysql; dbname=contactform; charset=utf8',
-      $db_username,
-      $db_password
-  );
-  $sql = 'select * from contacts';
-  $stmt = $pdo->prepare($sql);
-  $stmt->execute();
-  $results = $stmt->fetchall();
-
-  foreach ($results as $result) {
-      echo $result['title'];
-      echo '<br>';
-      echo '<br>';
-      echo $result['content'];
-      echo '<br>';
-      echo '<br>';
-  }
-  ?>
-
-  <p><a href="index.php">戻る</p>
-  </form>
+      <h2>送信履歴</h2>
+      <?php foreach ($contacts as $contact): ?>
+        <h3><?php echo $contact['title']; ?></h3>
+        <p><?php echo $contact['content']; ?></p>
+        <?php endforeach; ?>
+        <a href = "./index.php">戻る</a>
+    </div>
   </body>
 </html>
